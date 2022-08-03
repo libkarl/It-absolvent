91de61ac3df64c5d183ba50f158a5de637b83c93
Charles <karel.hb@email.cz>
2022-08-03 12:46:43 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import Tilt from 'react-parallax-tilt'
+import logo from './logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App '>
+      <div className='container mx-auto mt-10'>
+        <Tilt className='w-8/12 h-10 bg-slate-500 mx-auto'>
+          <div>
+            <h1>React Parallax Tilt 👀</h1>
+          </div>
+        </Tilt>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
