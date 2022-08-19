dceb32b14b6e6131e85e57eda9ef3ef642e51bf5
libkarl <karel.hb@email.cz>
2022-08-19 17:29:04 +0200

    delete function

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,22 +1,3 @@
 import React, { useEffect, useState } from 'react'
 
 export const delayDefinition = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
-export const useLocalStorage = <T>(key: string, defaultValue: T) => {
-  const [value, setValue] = useState<T>(() => {
-    let storage
-
-    try {
-      storage = JSON.parse(localStorage.getItem(key) || String(defaultValue))
-    } catch (error) {
-      storage = defaultValue
-    }
-
-    return storage
-  })
-
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value))
-  }, [value, key])
-
-  return [value, setValue] as const
-}
